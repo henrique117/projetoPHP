6ddcbaa7bdadb7792410483662606cb663af56b1
Henrique <hassis.moreira177@hotmail.com>
2022-04-12 10:33:36 -0300

    Foi sofrido demais

---
+++ projetoPHP/controller/Inicio.php
@@ -0,0 +1,18 @@
+<?php
+
+class Inicio
+{
+  private $message;
+  public function controller()
+  {
+    $inicio = new Template('view/inicio.html');
+    $inicio->set('inicio', 'Olá seja bem vindo!!!');
+    $this->message =  $inicio->saida();
+  }
+  public function getMessage()
+  {
+    return $this->message;
+  }
+}
+
+?>
